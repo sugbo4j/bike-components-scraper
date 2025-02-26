12d58e3bba8f781f4c62c2ea5b84eeb09e0dff9a
Joseph Gonzales <josephgonzales01@gmail.com>
2025-02-26 15:00:55 +1300

    Init build config

---
+++ src/main/java/nz/co/sugbo4j/bike_components_scraper/Application.java
@@ -0,0 +1,13 @@
+package nz.co.sugbo4j.bike_components_scraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/nz/co/sugbo4j/bike_components_scraper/BikeComponentsScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package nz.co.sugbo4j.bike_components_scraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeComponentsScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
