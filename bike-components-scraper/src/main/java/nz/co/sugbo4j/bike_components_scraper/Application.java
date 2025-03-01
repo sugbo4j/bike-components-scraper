70943e774ce54d9a8c471461148a9cd36e6db579
Joseph Gonzales <josephgonzales01@gmail.com>
2025-03-01 23:50:48 +1300

    scrape performancebike site

--- bike-components-scraper/src/main/java/nz/co/sugbo4j/bike_components_scraper/Application.java
+++ bike-components-scraper/src/main/java/nz/co/sugbo4j/bike_components_scraper/Application.java
@@ -1,13 +1,28 @@
 package nz.co.sugbo4j.bike_components_scraper;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+        Document doc;
+
+        try {
+            doc = Jsoup.connect("https://www.performancebike.com/mountain-bikes-bicycles/c14968").get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Elements items = doc.select("div.item");
+
+    }
 
 }
